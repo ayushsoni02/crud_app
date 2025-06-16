484214285d9566c01ef23b6e2f493984d61472d9
Ayush Soni <ayushsoni1021@gmail.com>
2025-06-16 18:27:55 +0530

    changes in UI

--- src/components/counter/counter-data-access.tsx
+++ src/components/counter/counter-data-access.tsx
@@ -9,6 +9,16 @@ import { useCluster } from '../cluster/cluster-data-access'
 import { useAnchorProvider } from '../solana/solana-provider'
 import { useTransactionToast } from '../use-transaction-toast'
 import { toast } from 'sonner'
+import { Signature } from 'lucide-react'
+import { error } from 'console'
+import { title } from 'process'
+
+
+interface CreateEntryArgs {
+  title: string;
+  message: string;
+  owner: PublicKey;
+}
 
 export function useCounterProgram() {
   const { connection } = useConnection()
@@ -28,26 +38,27 @@ export function useCounterProgram() {
     queryFn: () => connection.getParsedAccountInfo(programId),
   })
 
-  const initialize = useMutation({
-    mutationKey: ['counter', 'initialize', { cluster }],
-    mutationFn: (keypair: Keypair) =>
-      program.methods.initialize().accounts({ counter: keypair.publicKey }).signers([keypair]).rpc(),
-    onSuccess: async (signature) => {
-      transactionToast(signature)
-      await accounts.refetch()
-    },
-    onError: () => {
-      toast.error('Failed to initialize account')
-    },
-  })
-
+  const createEntry = useMutation<string, Error, CreateEntryArgs>({
+   mutationKey:[`journalEntry`,`create`, { cluster }],
+   mutationFn: async ({ title, message, owner }) => {
+    return program.methods.createJournalEntry(title, message).rpc();
+   },
+   onSuccess:(signature) => {
+    transactionToast(signature);
+    accounts.refetch();
+   },
+   onError:(error) => {
+    toast.error(`Error creating entry:${error.message}`);
+   },
+  });
+  
   return {
     program,
-    programId,
     accounts,
     getProgramAccount,
-    initialize,
-  }
+    createEntry,
+    programId
+  };
 }
 
 export function useCounterProgramAccount({ account }: { account: PublicKey }) {
@@ -58,12 +69,36 @@ export function useCounterProgramAccount({ account }: { account: PublicKey }) {
   const accountQuery = useQuery({
     queryKey: ['counter', 'fetch', { cluster, account }],
     queryFn: () => program.account.journelEntryState.fetch(account),
-  })
+  });
 
+ const updateEnrty = useMutation<string,Error, CreateEntryArgs>({
+  mutationKey: [`journalEntry`,`update`,{cluster}],
+  mutationFn: async({title,message})=> {
+    return program.methods.updateJournalEntry(title,message).rpc();
+  },
+  onSuccess: (signature) => {
+    transactionToast(signature);
+    accounts.refetch();
+  },
+  onError: (error)=>{
+    toast.error(`Error updating entry:${error.message}`);
+  },
+ });
 
+ const deleteEntry = useMutation({
+  mutationKey:[`journalEntry`,`delete`,{cluster}],
+  mutationFn: (title:string) => {
+    return program.methods.deleteJournalEntry(title).rpc();
+  },
+  onSuccess: (signature) => {
+    transactionToast(signature);
+    accounts.refetch();
+  },
+ });
 
-
-  return {
-    accountQuery,
-  }
+ return {
+  accountQuery,
+  updateEnrty,
+  deleteEntry,
+ };
 }
--- src/components/counter/counter-ui.tsx
+++ src/components/counter/counter-ui.tsx
@@ -1,20 +1,54 @@
 'use client'
 
 import { Keypair, PublicKey } from '@solana/web3.js'
-import { useMemo } from 'react'
 import { ExplorerLink } from '../cluster/cluster-ui'
 import { useCounterProgram, useCounterProgramAccount } from './counter-data-access'
 import { ellipsify } from '@/lib/utils'
 import { Button } from '@/components/ui/button'
 import { Card, CardContent, CardDescription, CardHeader, CardTitle } from '../ui/card'
+import { useState } from 'react'
+import { useWallet } from '@solana/wallet-adapter-react'
 
 export function CounterCreate() {
-  const { initialize } = useCounterProgram()
+  const [title,setTitle] = useState('');
+  const [message,setMessage] = useState('');
+  const {createEntry} = useCounterProgram();
+  const {publicKey} = useWallet();
+
+  const isFormValid = title.trim() !== '' && message.trim() !== '';
+
+ const handleSubmit = () => {
+  if (publicKey && isFormValid) {
+    console.log("Submitting Entry:", { title, message, owner: publicKey.toBase58() });
+    createEntry.mutateAsync({title,message,owner:publicKey});
+  }
+ };
+
+ if(!publicKey){
+  return <p>Connect Your Wallet</p>
+ }
 
   return (
-    <Button onClick={() => initialize.mutateAsync(Keypair.generate())} disabled={initialize.isPending}>
-      Create {initialize.isPending && '...'}
-    </Button>
+    <div>
+      <input type="text"
+      placeholder='Title'
+      value={title}
+      onChange={(e) => setTitle(e.target.value)}
+      className='input input-bordered w-full max-w-xs'
+      />
+      <textarea
+      placeholder='Message'
+      value={message}
+      onChange={(e)=> setMessage(e.target.value)}
+      className='textarea textarea-bordered w-full max-w-xs'
+      />
+      <button
+      type='submit'
+      onClick={handleSubmit}
+      disabled={createEntry.isPending || !isFormValid}
+      className='btn btn-xs lg:btn-md btn-primary hover:cursor-pointer border-2 border-primary'
+      >Create Journal Entry</button>
+    </div>
   )
 }
 
@@ -52,65 +86,64 @@ export function CounterList() {
 }
 
 function CounterCard({ account }: { account: PublicKey }) {
-  const { accountQuery, incrementMutation, setMutation, decrementMutation, closeMutation } = useCounterProgramAccount({
+  const { accountQuery,updateEnrty,deleteEntry} = useCounterProgramAccount({
     account,
-  })
+  });
 
-  const count = useMemo(() => accountQuery.data?.count ?? 0, [accountQuery.data?.count])
+  const {publicKey} = useWallet();
+  const [message,setMessage] = useState('');
+  const title = accountQuery.data?.title;
 
-  return accountQuery.isLoading ? (
-    <span className="loading loading-spinner loading-lg"></span>
-  ) : (
-    <Card>
-      <CardHeader>
-        <CardTitle>Counter: {count}</CardTitle>
-        <CardDescription>
-          Account: <ExplorerLink path={`account/${account}`} label={ellipsify(account.toString())} />
-        </CardDescription>
-      </CardHeader>
-      <CardContent>
-        <div className="flex gap-4">
-          <Button
-            variant="outline"
-            onClick={() => incrementMutation.mutateAsync()}
-            disabled={incrementMutation.isPending}
-          >
-            Increment
-          </Button>
-          <Button
-            variant="outline"
-            onClick={() => {
-              const value = window.prompt('Set value to:', count.toString() ?? '0')
-              if (!value || parseInt(value) === count || isNaN(parseInt(value))) {
-                return
-              }
-              return setMutation.mutateAsync(parseInt(value))
-            }}
-            disabled={setMutation.isPending}
-          >
-            Set
-          </Button>
-          <Button
-            variant="outline"
-            onClick={() => decrementMutation.mutateAsync()}
-            disabled={decrementMutation.isPending}
-          >
-            Decrement
-          </Button>
-          <Button
-            variant="destructive"
-            onClick={() => {
-              if (!window.confirm('Are you sure you want to close this account?')) {
-                return
-              }
-              return closeMutation.mutateAsync()
-            }}
-            disabled={closeMutation.isPending}
-          >
-            Close
-          </Button>
+  const isFormValid = message.trim() !== '';
+
+  const handleSubmit = () => {
+    if(publicKey && isFormValid && title){
+      updateEnrty.mutateAsync({title,message,owner:publicKey});
+    }
+  };
+
+  if(!publicKey){
+    return <p>Connect Your Wallet</p>
+  }
+
+  return accountQuery.isLoading ? ( 
+  <span className="loading loading-spinner loading-lg"></span>) : (
+    <div className='card card-bordered border-base-300 border-4 text-neutral-content'>
+      <div className='card-body items-center text-center'>
+        <div className='space-y-6'>
+        <h2 className='card-title justify-center text-3xl cursor-pointer'
+        onClick={()=> accountQuery.refetch()}
+        >{accountQuery.data?.title}
+        </h2>
+        <p>{accountQuery.data?.message}</p>
+        <div className='card-actions justify-around'>
+          <textarea
+           placeholder='Message'
+           value={message}
+           onChange={(e)=> setMessage(e.target.value)}
+           className='textarea textarea-bordered w-full max-xs'
+          />
+          <button 
+          onClick={handleSubmit}
+          disabled={updateEnrty.isPending || !isFormValid}
+          className='btn btn-xs lg:btn-md btn-primary'
+          >Update Journal Entry</button>
+          <button
+          onClick={()=> {
+            const title = accountQuery.data?.title;
+           if(title){
+            return deleteEntry.mutateAsync(title);
+           }
+          }
+          }
+          disabled={deleteEntry.isPending}
+          className="btn btn-xs lg:btn-md btn-error"
+          >Delete
+          </button>
         </div>
-      </CardContent>
-    </Card>
+        </div>
+      </div>
+    </div>
   )
+ 
 }
